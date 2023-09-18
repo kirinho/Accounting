0bbba1e0eadf526ce10d42bae31b88585efd1014
kirinho <lushukov04@gmail.com>
2023-09-18 14:15:22 +0300

    First commit

---
+++ src/test/java/com/petProject/Accounting/AccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petProject.Accounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
