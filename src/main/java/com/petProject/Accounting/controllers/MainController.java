58bc93bf7f1b4449bd4669faf20714814f9fa78f
kirinho <lushukov04@gmail.com>
2023-09-20 11:56:05 +0300

    Add login and register html docs, lately they will be upgraded

--- src/main/java/com/petProject/Accounting/controllers/MainController.java
+++ src/main/java/com/petProject/Accounting/controllers/MainController.java
@@ -10,5 +10,17 @@ public class MainController {
         model.addAttribute("greetings", "Main page");
         return "greeting";
     }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+//        model.addAttribute("greetings", "Main page");
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+//        model.addAttribute("greetings", "Main page");
+        return "register";
+    }
 }
 
